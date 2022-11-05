ac8d0365ab044c3572fed20b9134e42745390017
João Netto <jovinettonetto@gmail.com>
2022-11-05 13:59:43 +0000

    index atualizado

--- index.php
+++ index.php
@@ -21,13 +21,22 @@ $generos = $comando->fetchAll(PDO::FETCH_ASSOC);
     <title>biblioteca</title>
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <style>
+        a{
+            text-decoration: none;
+            color: white;
+            font-weight: bolder;
+        }
+    </style>
 </head>
 
 <body>
+
     <table class="table table-dark">
         <tr>
             <th>Id</th>
             <th>Nome</th>
+            <th><a href="insert.php"><button type="button" class="btn btn-secondary" >+</button></a></th>
         </tr>
         <?php foreach ($generos as $g) : ?>
             <tr>
